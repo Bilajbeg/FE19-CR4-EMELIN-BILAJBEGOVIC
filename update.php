86d7ee3144c99dd6f2de472c2f19cded6bbc1add
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-07-21 23:10:55 +0200

    finished coding

--- update.php
+++ update.php
@@ -31,7 +31,7 @@ if (isset($_POST["update"])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Update</title>
 </head>
 
 <body>
